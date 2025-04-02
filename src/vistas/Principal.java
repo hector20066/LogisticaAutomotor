2b56d056c4738938d2b703149c4faf35adf71e3b
hector20066 <hectorluisacevedocontreras00@gmail.com>
2025-04-02 13:24:02 -0500

    Se agrego un mensaje en la clase principal

--- src/vistas/Principal.java
+++ src/vistas/Principal.java
@@ -12,6 +12,7 @@ public class Principal {
     
     public static void main(String[]args){
         
+        System.out.println("Este es un mensaje");
         Automotor vista = new Automotor();
         ReporteVehiculos reporte = new ReporteVehiculos();
         
